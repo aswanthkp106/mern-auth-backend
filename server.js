c52f160dd664441a86cec5d7460b6f483d5917bc
aswanthkp106 <aswanthkp106@gmail.com>
2026-05-27 00:10:32 +0530

    Fixed Vercel backend

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.MONGO_URI);
-
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
 });
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+module.exports = app;
