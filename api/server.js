a7e342d2284ed54020300fe4d6cad46647821920
aswanthkp106 <aswanthkp106@gmail.com>
2026-05-27 22:31:01 +0530

    clean vercel backend

--- api/server.js
+++ api/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send("Backend Running Successfully");
 });
 
 module.exports = app;
