3a32ddf9dbc996fc409cf26077fe053f8fb07eb7
aswanthkp106 <aswanthkp106@gmail.com>
2026-05-27 20:26:48 +0530

    Minimal backend test

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-
-const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
 
 module.exports = app;
